4031681c0ed4c90f85b633a19048b3d12caf7d95
Techkidd <ronakverma2131@gmail.com>
2025-07-28 02:18:55 +0530

    connected fe to ws server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
   const [text, setText] = useState('');
   const [messages, setMessages] = useState<string[]>([]);
+  const wsRef = useRef<WebSocket>(null);
 
   const handleSend = () => {
-    if (text.trim() !== '') {
-      setMessages([...messages, text]);
+    if (text.trim() !== '' && wsRef.current) {
+      wsRef.current.send(JSON.stringify({
+        type: "chat",
+        text: text
+      }))
       setText('');
     }
   }
+
+  useEffect(() => {
+    wsRef.current = new WebSocket("ws://localhost:8080");
+
+    wsRef.current.onopen = () => {
+      console.log("Connected to websocket server")
+    }
+
+    wsRef.current.onmessage = (event) => {
+      const message = JSON.parse(event.data)  //json string to js object
+      if (message.type === "chat") {
+        setMessages(prev => [...prev, message.text]);
+      }
+    }
+
+    wsRef.current.onclose = () => {
+      console.log("disconnected from websocket server");
+    }
+
+
+  }, []);
 
   return (
     <>
